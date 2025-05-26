8b4ec4dd1a4d1fd7ab3f0838c79c79c834e28ee7
Dsadiq091 <daniyalsadiqueop@gmail.com>
2025-05-26 19:13:05 +0000

    Initial commit

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,15 @@
-# keep_alive.py
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
     return "I'm alive"
 
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
 def keep_alive():
-    # Use port 8080 for Replit
-    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
+    t = Thread(target=run)
+    t.start()
